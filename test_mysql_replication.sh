efc5e608625019d717f43e02b697bb01c3fca45c
Alon Dotan <alon1.dotan@gmail.com>
2018-07-06 20:12:06 +0000

    finish..

---
+++ test_mysql_replication.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Clean test table.."
+docker exec -it mysql_master mysql -uroot -prootpwd -D pipl -e "DROP TABLE dummy;"
+
+echo "Insert data to master..."
+docker exec -it mysql_master mysql -uroot -prootpwd -D pipl -e "create table dummy(a varchar(255));"
+
+for i in {1..10}; do
+	docker exec -it mysql_master mysql -uroot -prootpwd -D pipl -e "insert into dummy set a=$i;"
+done
+
+echo "Validating Slave..."
+docker exec -it mysql_master mysql -uroot -prootpwd -D pipl -e "select count(*) from dummy;"
+
